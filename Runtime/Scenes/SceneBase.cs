bc46bb14a21bdcb407167d25c575f4bdc60f1b5b
kuma3ism <kuma3ism@gmail.com>
2026-03-21 20:39:02 +0900

    リファクタリング

--- Runtime/Scenes/SceneBase.cs
+++ Runtime/Scenes/SceneBase.cs
@@ -71,7 +71,7 @@ namespace Ursa.Scenes
             // 子供が消えて自分が最前面になった時に呼ばれる
         }
 
-        protected sealed override void Update()
+        protected void Update()
         {
             if (_handleBackKey && IsTopScene && Input.GetKeyDown(KeyCode.Escape))
                 OnBackKeyPressed();
